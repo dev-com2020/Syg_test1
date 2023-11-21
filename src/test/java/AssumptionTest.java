a46567d0531d4dd751d5e02573a61350f13507ac
DEV-COM <kwp@tlen.pl>
2023-11-21 14:53:58 +0100

    stan na godz. 14:53

---
+++ src/test/java/AssumptionTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.*;
+
+public class AssumptionTest {
+
+    @Test
+    void assumeTrueTest(){
+        assumeTrue(false);
+        fail("Test 1 failed");
+    }
+
+    @Test
+    void assumeFalseTest(){
+        assumeFalse(this::getTrue);
+        fail("Test 2 failed");
+    }
+
+    private boolean getTrue(){
+        return true;
+    }
+
+    @Test
+    void assumingThatTest(){
+        assumingThat(false, ()-> fail("Test 3 failed"));
+    }
+}
