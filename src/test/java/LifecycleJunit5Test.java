dcc0603171bdd1586f52b8867e917e6d97971d9a
DEV-COM <kwp@tlen.pl>
2023-11-21 12:53:03 +0100

    stan na godz. 12:52

---
+++ src/test/java/LifecycleJunit5Test.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.*;
+
+//@Disabled("All test in this class wille be disabled")
+class LifecycleJunit5Test {
+
+    @BeforeAll
+    static void setupAll(){
+        System.out.println("Setup ALL TESTS in the class");
+    }
+
+    @BeforeEach
+    void setup(){
+        System.out.println("Setup EACH TEST in the class");
+    }
+
+    @Test
+    void testOne(){
+        System.out.println("TEST 1");
+    }
+
+    @Test
+    @DisplayName("Custom test name with spaces and emo \uD83E\uDD78")
+    void testTwo(){
+        System.out.println("TEST 2");
+    }
+
+    @AfterEach
+    void teardown(){
+        System.out.println("Teardown EACH TEST in the class");
+    }
+
+    @AfterAll
+    static void teardownAll(){
+        System.out.println("Teardown ALL TESTS in the class");
+    }
+
+}
---
+++ src/test/java/MyFirstJunitTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyFirstJunitTest {
+    @Test
+    void myFirstTest(){
+        String message = "1+1 should be equal to 2";
+        System.out.println(message);
+        assertEquals(2, 1 + 1, message);
+    }
+    @Test
+    void standardAssertions(){
+        assertEquals(2,2);
+        assertTrue(true,"Opcjonalna wiadomość");
+        assertFalse(false, ()-> "Really " + "expensive "+ "message" + ".");
+    }
+
+    @Test
+    void groupedAssertions() {
+        Address address = new Address("John", "Smith");
+        assertAll("address", ()-> assertEquals("John",
+                address.getFirstName()),
+                ()-> assertEquals("Smith", address.getLastName()));
+    }
+
+    @Test
+    void exceptionTesting(){
+        Throwable exception =
+                assertThrows(IllegalArgumentException.class, ()-> {
+                    throw new IllegalArgumentException("wiadomość");});
+        assertEquals("wiadomość", exception.getMessage());
+    }
+}
