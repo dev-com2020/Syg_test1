d1635b719152be890646354bb46a3dfba00a8ba0
DEV-COM <kwp@tlen.pl>
2023-11-22 09:25:52 +0100

    II część, stan na godz. 09:25

---
+++ src/main/java/LoginException.java
@@ -0,0 +1,7 @@
+public class LoginException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public LoginException(String cause){
+        super(cause);
+    }
+}
