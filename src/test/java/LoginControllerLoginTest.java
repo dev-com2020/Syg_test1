68df2da3ace9450e1300ff008aa7243c02056a60
DEV-COM <kwp@tlen.pl>
2023-11-22 09:56:50 +0100

    II część, stan na godz. 09:56

--- src/test/java/LoginControllerLoginTest.java
+++ src/test/java/LoginControllerLoginTest.java
@@ -3,34 +3,50 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 
 import javax.security.auth.login.LoginException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LoginControllerLoginTest {
-    @InjectMocks
-    LoginController loginController;
 
-    @Mock
+//    @InjectMocks
+//    LoginController loginController;
+
+    @InjectMocks
     LoginService loginService;
 
+//    @Mock
+//    LoginService loginService;
+    @Spy
+        LoginRepository loginRepository;
+
     UserForm userForm = new UserForm("tomek","pass");
+    UserForm userOK = new UserForm("user1","p1");
+
+//    @Test
+//    void testLoginOk() throws LoginException {
+//        when(loginService.login(userForm)).thenReturn(true);
+//        String responseLogin = loginController.login(userForm);
+//        assertEquals("OK", responseLogin);
+//        verify(loginService).login(userForm);
+//        verifyNoMoreInteractions(loginService);
+//    }
+//
+//    @Test
+//    void testLoginError(){
+//        assertEquals("ERROR", loginController.login(null));
+//    }
 
     @Test
-    void testLoginOk() throws LoginException {
-        when(loginService.login(userForm)).thenReturn(true);
-        String responseLogin = loginController.login(userForm);
-        assertEquals("OK", responseLogin);
-        verify(loginService).login(userForm);
-        verifyNoMoreInteractions(loginService);
+    void testLoginOK1() throws LoginException {
+        assertTrue(loginService.login(userOK));
     }
 
-    @Test
-    void testLoginError(){
-        assertEquals("ERROR", loginController.login(null));
-    }
+
 
 }
