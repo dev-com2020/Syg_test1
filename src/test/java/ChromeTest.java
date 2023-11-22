761c6cd226e187eafc7002c1dc6b20f5566ca909
DEV-COM <kwp@tlen.pl>
2023-11-22 10:41:20 +0100

    II część, stan na godz. 10:41

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupDriverManager() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupChromeDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void goToGooglePage() {
+        driver.get("https://www.google.pl/");
+        String actual = driver.getCurrentUrl();
+        assertEquals("https://www.google.pl/", actual);
+    }
+}
